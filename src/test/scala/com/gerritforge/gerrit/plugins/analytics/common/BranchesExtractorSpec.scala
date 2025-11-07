35575c4cad28c41eaac3dac81c27e607013f7a76
Luca Milanesio <luca.milanesio@gmail.com>
2025-11-07 18:24:10 +0000

    Update base package to com.gerritforge and add BSL license prefix
    Change-Id: I061bd5eba86b42da163aecf731b1752408624890

---
+++ src/test/scala/com/gerritforge/gerrit/plugins/analytics/common/BranchesExtractorSpec.scala
@@ -0,0 +1,30 @@
+package com.gerritforge.gerrit.plugins.analytics.common
+
+// Copyright (C) 2025 GerritForge, Inc.
+//
+// Licensed under the BSL 1.1 (the "License");
+// you may not use this file except in compliance with the License.
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+  behavior of "branchesOfCommit"
+
+  it should "extract one branch for a commit existing only in one branch" in {
+    testFileRepository.commitFile("file", "content")
+    testFileRepository.branch("feature/branch", "master")
+    val commit = testFileRepository.commitFile("fileOnBranch", "content2", branch = "feature/branch")
+
+    commitsBranches.branchesOfCommit(commit) shouldBe Set("feature/branch")
+  }
+
+  it should "extract two branches for a commit existing in two different branches" in {
+    val commit = testFileRepository.commitFile("file", "content")
+    testFileRepository.branch("feature/branch", "master")
+    testFileRepository.commitFile("fileOnBranch", "content2", branch = "feature/branch")
+
+    commitsBranches.branchesOfCommit(commit) shouldBe Set("feature/branch", "master")
+  }
+}
